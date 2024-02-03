c0710ec5bc526308075c73e40c4303d148375e00
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-03 14:40:30 +1100

    added my schemas

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import works from "./works"
+import workExperience from "./workExperience"
+import skills from "./skills"
+import experiences from "./experiences"
+import contact from "./contact"
+import abouts from "./abouts"
+
+export const schemaTypes = [works, workExperience, skills, experiences, contact, abouts]
