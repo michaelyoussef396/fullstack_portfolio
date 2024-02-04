0f00f65f590fd478e45ad8617ebcce4c5b8766bd
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-04 15:13:50 +1100

    create the layout for the project

--- frontend_portfolio/src/App.js
+++ frontend_portfolio/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { About, Footer, Header, Skills,Work } from './container'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
     </div>
   )
 }
