1d9c3f63fc9a4a5acb2ef4bdaa5ac3041077f227
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-05 06:41:20 +1100

    added header/hero page

--- frontend_portfolio/src/constants/images.js
+++ frontend_portfolio/src/constants/images.js
@@ -1,11 +1,25 @@
-import logo from '../assets/logo.png'
 import michael from '../assets/michael.png'
 import youssef from '../assets/youssef.png'
 import bgIMG from '../assets/bgIMG.png'
+import circle from '../assets/circle.svg'
+import sass from '../assets/sass.png'
+import react from '../assets/react.png'
+import python from '../assets/python.png'
+import resume_icon from '../assets/resume_icon.png'
+import github_icon from '../assets/github_icon.png'
+import medium_icon from '../assets/medium_icon.png'
+import linkedin_icon from '../assets/linkedin_icon.png'
 
 export default {
-    logo,
     michael,
     youssef,
-    bgIMG
+    bgIMG,
+    circle,
+    sass,
+    react,
+    python,
+    resume_icon,
+    github_icon,
+    linkedin_icon,
+    medium_icon
 }
--- frontend_portfolio/src/container/Header/Header.jsx
+++ frontend_portfolio/src/container/Header/Header.jsx
@@ -1,10 +1,85 @@
 import React from 'react'
 import './Header.scss'
+import { images } from '../../constants' 
+import {motion} from 'framer-motion'
+
+const scaleVariant = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
+const linksAndIcons = [
+  { icon: images.resume_icon, url: '../assets/michael_resume.pdf' },
+  { icon: images.github_icon, url: 'https://github.com/michaelyoussef396' },
+  { icon: images.linkedin_icon, url: 'https://www.linkedin.com/in/michael-youssef-93625327a/' },
+  { icon: images.medium_icon, url: 'https://medium.com/@michaelyoussef396' },
+];
+
 
 const Header = () => {
+  
   return (
-    <div>
-      
+    <div id='home' className='app__header app__flex'>
+      <motion.div
+      whileInView={{x: [-100, 0], opacity: [0,1] }}
+      transition={{duration: 0.5}}
+      className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cpm app__flex'>
+            <span>👋</span>
+            <div style={{marginLeft: 20}}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Michael</h1>
+            </div>
+          </div>
+
+          <div className='tag-cmp app__flex'>
+            <p>Full-Stack Developer</p>
+            <p>Web Developer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+      whileInView={{opacity: [0,1] }}
+      transition={{duration: 0.5, delayChildren: 0.5}}
+      className='app__header-img'
+      >
+        {linksAndIcons.map((linkAndIcon, index) => (
+          <div className='circle-cmp app__flex' key={`icon-${index}`}>
+            <a href={linkAndIcon.url} target="_blank" rel="noopener noreferrer">
+              <img src={linkAndIcon.icon} alt='icon' />
+            </a>
+          </div>
+        ))}
+        <motion.img
+        whileInView={{scaleX: [0,1] }}
+        transition={{duration: 1, ease: 'easeInOut'}}
+        className='overlay circle'
+        src={images.circle}
+        alt='background-circle'
+        />
+
+      </motion.div>
+
+      <motion.div
+      variant={scaleVariant}
+      whileInView={scaleVariant.whileInView}
+      className='app__header-circles'
+      >
+        {[images.sass, images.python, images.react].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt='circle' />
+          </div>
+        ))}
+      </motion.div>
     </div>
   )
 }
