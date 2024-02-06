fe57bb294c20479eee4de314b12855ac8f64dfe7
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-06 15:18:09 +1100

    updated abouts section

--- frontend_portfolio/src/constants/images.js
+++ frontend_portfolio/src/constants/images.js
@@ -10,6 +10,10 @@ import github_icon from '../assets/github_icon.png';
 import medium_icon from '../assets/medium_icon.png';
 import linkedin_icon from '../assets/linkedin_icon.png';
 import profile from '../assets/profile.png';
+import about1 from '../assets/about1.png'
+import about2 from '../assets/about2.png'
+import about3 from '../assets/about3.png'
+import about4 from '../assets/about4.png'
 
 const images = {
     michael,
@@ -23,7 +27,11 @@ const images = {
     github_icon,
     linkedin_icon,
     medium_icon,
-    profile
+    profile,
+    about1,
+    about2,
+    about3,
+    about4
 }
 
 export default images;
--- frontend_portfolio/src/container/About/About.jsx
+++ frontend_portfolio/src/container/About/About.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './About.scss'
 import {motion} from 'framer-motion';
+import { images } from '../../constants';
 
+const abouts = [
+  {title: 'Frontend Development', description: 'I am good Frontend Developer', imgURL: images.about1},
+  {title: 'backend Development', description: 'I am good backend developer', imgURL: images.about2},
+  {title: 'fullstack Development', description: 'I am good fullstack developer', imgURL: images.about3},
+  {title: 'freelancer', description: 'I am good freelancer', imgURL: images.about4}
+]
 
 const About = () => {
   return (
     <>
-      <h2 className='head-text'>
-         I Know That
-         <span>Good Developement</span>
-         <br />
-         means
-        <span>Good Business</span>
-      </h2>
+      <h2 className='head-text'>I Know That <span>Good Developement</span> <br />means <span>Good Business</span></h2>
 
       <div className='app__profiles'>
-
+        {abouts.map((about, index) => (
+          <motion.div
+          whileInView={{opacity: 1}}
+          whileHover={{scale: 1.1}}
+          transition={{duration: 0.5, type: 'tween'}}
+          className='app__profile-item'
+          key={about.title + index}
+          >
+            <img src={about.imgURL} alt={about.title}/>
+            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+          </motion.div>
+        ))}
       </div>
     </>
   )
