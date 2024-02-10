bc9cd725cecb2b324fa1189ebffc993118105555
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-02-10 19:49:34 +1100

    Fixed skill icon reference in Skills component, enhanced data fetching for dynamic content, and implemented framer-motion for UI transitions.

--- frontend_portfolio/src/container/Skills/Skills.jsx
+++ frontend_portfolio/src/container/Skills/Skills.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Skills.scss'
+import {motion} from 'framer-motion'
+import AppWrap from '../../wrapper/AppWrap';
+import {urlFor, client} from '../../client'
+import ReactToolTip from 'react-tooltip'
 
 const Skills = () => {
+
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h2 className='head-text'>Skills & Experience</h2>
+
+      <div className='app__skills-container'>
+      <motion.div className='app__skills-list'>
+        {skills.map((skill) => ( // Changed from Skills to skills
+          <motion.div
+            whileInView={{opacity: [0, 1]}}
+            transition={{duration: 0.5}}
+            className='app__skills-item app__flex' // Corrected class name from 'app__skils-item' to 'app__skills-item'
+            key={skill.name}
+          >
+            <div className='app__flex' style={{backgroundColor: skill.bgColor}}>
+              <img src={urlFor(skill.icon)} alt={skill.name} /> // Changed from skills.icon to skill.icon
+            </div>
+
+            <p className='p-text'>{skill.name}</p>
+          </motion.div>
+          ))}
+          </motion.div>
+
+      </div>
+    </>
   )
 }
 
